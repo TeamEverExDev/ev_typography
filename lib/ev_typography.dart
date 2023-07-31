3cbc29e9ff34555c3277db402aa3ca0f7b18eb9b
kim dong joo <50819258+redhotsixbull@users.noreply.github.com>
2023-07-31 12:41:12 +0900

    update font

--- lib/ev_typography.dart
+++ lib/ev_typography.dart
@@ -5,87 +5,155 @@ import 'package:flutter/material.dart';
 class EvTypography {
   static TextStyle heading1 = const TextStyle(
     fontSize: 48,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle heading2 = const TextStyle(
     fontSize: 44,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle heading3 = const TextStyle(
     fontSize: 40,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle heading4 = const TextStyle(
     fontSize: 36,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle heading5 = const TextStyle(
     fontSize: 32,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle heading6 = const TextStyle(
     fontSize: 28,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle title1 = const TextStyle(
     fontSize: 26,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle title2 = const TextStyle(
     fontSize: 24,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle title3 = const TextStyle(
     fontSize: 22,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle title4 = const TextStyle(
     fontSize: 20,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle body1 = const TextStyle(
     fontSize: 18,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle body2 = const TextStyle(
     fontSize: 17,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle body3 = const TextStyle(
     fontSize: 16,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle body4 = const TextStyle(
     fontSize: 15,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle detail1 = const TextStyle(
     fontSize: 14,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle detail2 = const TextStyle(
     fontSize: 13,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 
   static TextStyle detail3 = const TextStyle(
     fontSize: 12,
-    fontFamily: "test",
+    fontFamily: "Pretendard",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    textBaseline: TextBaseline.ideographic,
+    leadingDistribution: TextLeadingDistribution.even,
   );
 }
 
