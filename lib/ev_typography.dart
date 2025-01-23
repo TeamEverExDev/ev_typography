7f3f809aa5847fa6a064850a5279a01870333c83
Hussien <hussien.elmi@outlook.com>
2025-01-23 18:23:24 +0900

    fix: 변수 type 업데이트

--- lib/ev_typography.dart
+++ lib/ev_typography.dart
@@ -3,97 +3,97 @@ library ev_typography;
 import 'package:flutter/material.dart';
 
 class EvTypography {
-  static MoraTextStyle headline1 = MoraLargeTextStyle(
+  static MoraLargeTextStyle headline1 = MoraLargeTextStyle(
     fontSize: 36,
     lineHeight: 48,
     letterSpacing: -0.027,
   );
 
-  static MoraTextStyle headline2 = MoraLargeTextStyle(
+  static MoraLargeTextStyle headline2 = MoraLargeTextStyle(
     fontSize: 28,
     lineHeight: 38,
     letterSpacing: -0.0236,
   );
 
-  static MoraTextStyle headline3 = MoraLargeTextStyle(
+  static MoraLargeTextStyle headline3 = MoraLargeTextStyle(
     fontSize: 24,
     lineHeight: 32,
     letterSpacing: -0.023,
   );
 
-  static MoraTextStyle title1 = MoraSmallTextStyle(
+  static MoraSmallTextStyle title1 = MoraSmallTextStyle(
     fontSize: 22,
     lineHeight: 31,
     letterSpacing: -0.0194,
   );
 
-  static MoraTextStyle title2 = MoraSmallTextStyle(
+  static MoraSmallTextStyle title2 = MoraSmallTextStyle(
     fontSize: 20,
     lineHeight: 28,
     letterSpacing: -0.012,
   );
 
-  static MoraTextStyle heading1 = MoraSmallTextStyle(
+  static MoraSmallTextStyle heading1 = MoraSmallTextStyle(
     fontSize: 18,
     lineHeight: 26,
     letterSpacing: -0.002,
   );
 
-  static MoraTextStyle heading2 = MoraSmallTextStyle(
+  static MoraSmallTextStyle heading2 = MoraSmallTextStyle(
     fontSize: 17,
     lineHeight: 24,
     letterSpacing: 0.0,
   );
 
-  static MoraTextStyle body1Normal = MoraSmallTextStyle(
+  static MoraSmallTextStyle body1Normal = MoraSmallTextStyle(
     fontSize: 16,
     lineHeight: 24,
     letterSpacing: 0.0057,
   );
 
-  static MoraTextStyle body2Normal = MoraSmallTextStyle(
+  static MoraSmallTextStyle body2Normal = MoraSmallTextStyle(
     fontSize: 15,
     lineHeight: 22,
     letterSpacing: 0.0096,
   );
 
-  static MoraTextStyle body1Reading = MoraSmallTextStyle(
+  static MoraSmallTextStyle body1Reading = MoraSmallTextStyle(
     fontSize: 16,
     lineHeight: 26,
     letterSpacing: 0.0057,
   );
 
-  static MoraTextStyle body2Reading = MoraSmallTextStyle(
+  static MoraSmallTextStyle body2Reading = MoraSmallTextStyle(
     fontSize: 15,
     lineHeight: 24,
     letterSpacing: 0.0096,
   );
 
-  static MoraTextStyle label1Normal = MoraSmallTextStyle(
+  static MoraSmallTextStyle label1Normal = MoraSmallTextStyle(
     fontSize: 14,
     lineHeight: 20,
     letterSpacing: 0.0145,
   );
 
-  static MoraTextStyle label1Reading = MoraSmallTextStyle(
+  static MoraSmallTextStyle label1Reading = MoraSmallTextStyle(
     fontSize: 14,
     lineHeight: 22,
     letterSpacing: 0.0145,
   );
 
-  static MoraTextStyle label2 = MoraSmallTextStyle(
+  static MoraSmallTextStyle label2 = MoraSmallTextStyle(
     fontSize: 13,
     lineHeight: 18,
     letterSpacing: 0.0194,
   );
 
-  static MoraTextStyle caption1 = MoraSmallTextStyle(
+  static MoraSmallTextStyle caption1 = MoraSmallTextStyle(
     fontSize: 12,
     lineHeight: 16,
     letterSpacing: 0.0252,
   );
 
-  static MoraTextStyle caption2 = MoraSmallTextStyle(
+  static MoraSmallTextStyle caption2 = MoraSmallTextStyle(
     fontSize: 11,
     lineHeight: 14,
     letterSpacing: 0.0311,
